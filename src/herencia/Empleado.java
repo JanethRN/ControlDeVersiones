417adfb053b9cfcf1311e986cfe45f837fad4a87
DELL <DELL@DESKTOP-9MPN42T>
2021-02-26 16:28:11 -0500

    Segundo commit

--- src/herencia/Empleado.java
+++ src/herencia/Empleado.java
@@ -11,18 +11,43 @@ package herencia;
  */
 public class Empleado {
     
-    //---------------------------------------------
+    //--------------------------------------------------------------------------
     //Atributos   Jordan Espinosa-Julissa Renteria
-    //---------------------------------------------
+    //--------------------------------------------------------------------------
     
      private String nombre;
 
-    //-----------------------------------------------------------
+    //--------------------------------------------------------------------------
     //Constructor por defecto  Jordan Espinosa-Julissa Renteria
-    //----------------------------------------------------------
+    //--------------------------------------------------------------------------
      
     public Empleado() {
     }
-
     
+    //--------------------------------------------------------------------------
+    //Constructor con un parámetro  Jordan Espinosa-Julissa Renteria
+    //--------------------------------------------------------------------------
+    public Empleado(String nombre) {
+        this.nombre = nombre;
+        System.out.println("Constructor de Empleado " + nombre);
+    }
+   
+    //--------------------------------------------------------------------------
+    //Métodos get y set Jordan Espinosa-Julissa Renteria
+    //--------------------------------------------------------------------------
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    //--------------------------------------------------------------------------
+    //Método toString() Jordan Espinosa-Julissa Renteria
+    //--------------------------------------------------------------------------
+                                  
+    @Override
+    public String toString() {
+        return "Empleado " + nombre;
+    }
 }
