3880b7dcd84c18b458299b8200d4b9ce562ba81a
DELL <DELL@DESKTOP-9MPN42T>
2021-02-26 17:06:21 -0500

    Primer commit

---
+++ src/programacionporparejas/Encapsulamiento.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacionporparejas;
+
+/**
+ *
+ * @author DELL
+ */
+public class Encapsulamiento {
+    
+    //--------------------------------------------------------------------------
+    //Atributos   Jordan Espinosa-Julissa Renteria
+    //--------------------------------------------------------------------------
+    
+    private String nombre;
+    private int patas;
+    
+
+}
