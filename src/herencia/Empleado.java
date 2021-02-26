69c840dddb1395119e3dfdddfefd4d7bafc8650a
DELL <DELL@DESKTOP-9MPN42T>
2021-02-26 16:25:42 -0500

    Primer commit

---
+++ src/herencia/Empleado.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author DELL
+ */
+public class Empleado {
+    
+    //---------------------------------------------
+    //Atributos   Jordan Espinosa-Julissa Renteria
+    //---------------------------------------------
+    
+     private String nombre;
+
+    //-----------------------------------------------------------
+    //Constructor por defecto  Jordan Espinosa-Julissa Renteria
+    //----------------------------------------------------------
+     
+    public Empleado() {
+    }
+
+    
+}
