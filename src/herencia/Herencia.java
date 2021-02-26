66f0e8bed0ba9071087ec722ec29f0b92cf5dde5
DELL <DELL@DESKTOP-9MPN42T>
2021-02-26 16:39:39 -0500

    Quinto commit

---
+++ src/herencia/Herencia.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author DELL
+ */
+public class Herencia {
+    
+    public static void main (String[]args){
+        
+        Oficial objOficial = new Oficial("Jorge");
+        Empleado objEmple = new Empleado("Julissa");
+        Operario objOperario = new Operario("Jordan");
+        
+    }
+    
+}
