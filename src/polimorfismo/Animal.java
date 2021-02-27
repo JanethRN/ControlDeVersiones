c9193ab6492fe3bae5e301e15f7f12681017b6d8
DELL <DELL@DESKTOP-9MPN42T>
2021-02-27 14:58:36 -0500

    Primer commit Jordan

---
+++ src/polimorfismo/Animal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfismo;
+
+/**
+ *
+ * @author Jordan
+ */
+public class Animal {
+    
+    //--------------------------------------------------------------------------
+    //Atributos  Jordan Espinosa-Julissa Renteria
+    //--------------------------------------------------------------------------
+    
+    private String nombre,color;
+    
+}
